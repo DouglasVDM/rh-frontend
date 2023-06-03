ce6be494a6645ce60d9e7081779f28b9f121e54c
DouglasVDM <douglasvdmerwe@gmail.com>
2023-06-03 16:54:10 +0200

    update nouns select dropdown

--- src/components/Nouns.js
+++ src/components/Nouns.js
@@ -1,33 +1,31 @@
+import React, { useState } from "react";
+import Select from "react-select";
 
-import React, {  useState } from 'react'
-import Select from 'react-select';
+const ListNouns = ({ nouns, onSelectNoun }) => {
+  const [selectedNoun, setSelectedNoun] = useState(null);
 
-const ListNouns = ({nouns, onSelectNoun}) => {
-    const [selectedNoun, setSelectedNoun] = useState(null);
+  const nounOptions = nouns.map((noun) => ({
+    value: noun.noun_id,
+    label: noun.noun_name,
+  }));
 
-    const nounOptions = nouns.map((noun)=>({
-        value:noun.noun_id,
-        label:noun.noun_name
-    }));
+  const handleSelectChange = (noun) => {
+    setSelectedNoun(noun);
+    onSelectNoun(noun.label);
+  };
 
-    const handleSelectChange = (noun)=>{
-        setSelectedNoun(noun)
-        onSelectNoun(noun.label)
-    };
-
-    return (
-<div className="container">
-    <h1>Nouns Dropdown</h1>
-  <Select
-   value={selectedNoun}
-   options={nounOptions}
-   onChange={handleSelectChange}
-   className="react-select-container"
-   classNamePrefix="react-select"
-   />
-   </div>
-   );
+  return (
+    <div className="container">
+      <h1>Nouns Dropdown</h1>
+      <Select
+        value={selectedNoun}
+        options={nounOptions}
+        onChange={handleSelectChange}
+        className="react-select-container"
+        classNamePrefix="react-select"
+      />
+    </div>
+  );
 };
-    
-    export default ListNouns
-    
+
+export default ListNouns;
