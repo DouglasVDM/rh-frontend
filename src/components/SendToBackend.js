d6060c2a131c78ab999f1a31129b832513101532
DouglasVDM <douglasvdmerwe@gmail.com>
2023-06-03 17:34:30 +0200

    update class property to className

--- src/components/SendToBackend.js
+++ src/components/SendToBackend.js
@@ -7,7 +7,7 @@ const SendToBackend = ({ selectedNoun }) => {
       const body = { sentence_name };
       console.log("selectedNoun", selectedNoun);
       setSentence_name(selectedNoun);
-      
+
       // Need to find a way around hardcoding this url. When clicking the send button, the url used is http://localhost:3000/indefined/sentences and not the baseUrl specified by in App.js.
       const response = await fetch("http://localhost:5000/api/v1/sentences", {
         method: "POST",
@@ -25,7 +25,7 @@ const SendToBackend = ({ selectedNoun }) => {
   };
 
   return (
-    <div class="col-md-12 text-center">
+    <div className="col-md-12 text-center">
       <button className="btn btn-primary " onClick={sendToBackend}>
         Send to Backend
       </button>
