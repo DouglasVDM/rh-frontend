f8e527670c8204c8acf5ed58384f41f5d0574df8
DouglasVDM <douglasvdmerwe@gmail.com>
2023-06-04 23:28:07 +0200

    update previoussentences component

--- src/components/PreviousSentences.js
+++ src/components/PreviousSentences.js
@@ -5,7 +5,7 @@ const PreviousSentences = ({ sentences }) => {
     <div>
       <h3 className="text-center mt-5">Previous Sentences</h3>
       <ul>
-        {sentences.map((sentences,index) => (
+        {sentences.map((sentences, index) => (
           <p key={index}>{sentences.id}. {sentences.sentence}</p>
         ))}
       </ul>
