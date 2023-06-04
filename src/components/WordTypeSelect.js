f93caa4679769147d5c6bb8116245ef4eef6f36e
DouglasVDM <douglasvdmerwe@gmail.com>
2023-06-04 23:04:11 +0200

    update wordtypeselect component

--- src/components/WordTypeSelect.js
+++ src/components/WordTypeSelect.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 
 const WordTypeSelect = ({ wordTypes, onWordTypeChange }) => {
-  return (
-    <Form.Select onChange={onWordTypeChange}>
-      <option value="">Select word type</option>
-      {/* Render the available word types */}
-      {wordTypes.map((wordType) => (
-        <option key={wordType.id} value={wordType.id}>
-          {wordType.word_type}
-        </option>
-      ))}
-    </Form.Select>
-  );
+    return (
+        <Container>
+            <p>Start by choosing your words</p>
+            <Form.Select onChange={onWordTypeChange}>
+                <option value="">Select a word by type</option>
+                {/* Render the available word types */}
+                {wordTypes.map((wordType) => (
+                    <option key={wordType.id} value={wordType.word}>
+                        {wordType.word_type} : {wordType.word}
+                    </option>
+                ))}
+            </Form.Select>
+        </Container>
+    );
 };
 
 export default WordTypeSelect;
