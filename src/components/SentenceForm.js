2ca80b38e60d98e78f91aa544a8b8a2652eae9f8
DouglasVDM <douglasvdmerwe@gmail.com>
2023-06-04 23:05:17 +0200

    update sentenceform

--- src/components/SentenceForm.js
+++ src/components/SentenceForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const SentenceForm = ({ onSubmit }) => {
+const SentenceForm = ({ onSubmit, sentence }) => {
   return (
     <div>
+      <p>Finally review your sentence then submit <br /> <i>{sentence}</i></p>
       <Button onClick={onSubmit}>Submit Sentence</Button>
     </div>
   );
