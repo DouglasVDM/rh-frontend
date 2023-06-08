8f9b3de785fc76af40d65190d2ba2c97018f38b6
Douglas Van Der Merwe <douglasvdmerwe@gmail.com>
2023-06-08 19:55:44 +0200

    set selected type to empty string
    Clear the wordType select

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ function App() {
   const handleAddWord = () => {
     if (selectedWord) {
       setSentence((prevSentence) => prevSentence + " " + selectedWord);
+      selectedType("");
       setSelectedWord("");
     }
   };
